fc382cf67c1028b3e185dd18ddc40534c2e20498
stephjin <stephanie.k.jin@gmail.com>
2014-11-09 14:23:32 -0800

    add css styling.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
   has_secure_password
+  field :name, type: String
   field :email, type: String
   field :password_digest, type: String
 
   validates_uniqueness_of :email
+  validates_presence_of :email, :password_digest
 end
